1b6c62018d802e6238eb1b95f341c1cd0dd3678d
Devansh Mishra <m.devansh0903@gmail.com>
2026-07-28 19:44:50 +0530

    Add real Google client ID

--- script.js
+++ script.js
@@ -405,7 +405,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     // Replace with your actual Google Client ID (same one used in your backend .env)
-    const GOOGLE_CLIENT_ID = 'YOUR_GOOGLE_CLIENT_ID_HERE';
+    GOOGLE_CLIENT_ID=798912287316-aoir8ucttobi2mhe0tpv1ah5t9fft0dj.apps.googleusercontent.com
 
     if (window.google) {
         google.accounts.id.initialize({
